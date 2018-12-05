d7f4039264a5f068326f1cbfe45b817ca33db552
shimaanaga <shimaa_naga@yahoo.com>
2018-12-06 00:07:23 +0200

    update design that show to users

--- resources/views/website/bu/all.blade.php
+++ resources/views/website/bu/all.blade.php
@@ -20,6 +20,17 @@
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <!------ Include the above in your HEAD tag ---------->
 
+
+
+
+{{--
+    <!-- Bootstrap 3.3.7 -->
+    <link rel="stylesheet" href="{{asset('admin/bower_components/bootstrap/dist/css/bootstrap.min.css')}}">
+    <!-- Font Awesome -->
+
+--}}
+    
+
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
 
@@ -28,8 +39,34 @@
         <div class="row">
             <div class="col-12 col-sm-3">
                 <div class="card bg-light mb-3">
-                    <div class="card-header bg-primary text-white text-uppercase"><i class="fa fa-list"></i> Categories</div>
+                    <div class="card-header bg-primary text-white text-uppercase"><i class="fa fa-list"></i> Select Building</div>
+
+
+
                     <ul class="list-group category_block">
+
+                        {{--
+                        <!-- search form -->
+                        <form action="#" method="get" class="sidebar-form" style="display:inline-block; padding: 5px 40px 5px 3px; ">
+                            <div class="input-group">
+                                <input type="text" name="q" class="form-control" placeholder="Search...">
+                                    <span class="input-group-btn">
+                                         <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+                                         </button>
+                                    </span>
+                            </div>
+                        </form>
+                        <!-- /.search form -->
+
+                        --}}
+                        <li class="list-group-item">
+                            {!! Form::text("price_form" , null ,['class' => 'form-control' , 'placeholder' => 'Building Price']) !!}
+                        </li>
+                        <li class="list-group-item">
+                            {!! Form::select("rooms" ,['3','4','5','6','7','8','9'] , null ,['class' => 'form-control' , 'placeholder' => 'No.Rooms']) !!}
+                        </li>
+
+
                         <li class="list-group-item"><a href="{{url('/showAllBuildings')}}"> All Buildings </a></li>
                         <li class="list-group-item"><a href="{{url('/ForRent')}}">Rent</a></li>
                         <li class="list-group-item"><a href="{{url('/Buy')}}"> Buy </a></li>
